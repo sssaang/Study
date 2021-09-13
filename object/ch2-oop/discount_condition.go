8377c44a728cc84566a27bf811e6c46e90e727a9
Sangmin Kim <sangmin.kim@business-canvas.com>
2021-09-14 03:28:11 +0900

    feat: add discount policy and condition

--- object/ch2-oop/discount_condition.go
+++ object/ch2-oop/discount_condition.go
@@ -1,3 +1,15 @@
 package main
 
-type DiscountCondition interface{}
+type DiscountCondition interface{
+	isSatisfied() bool
+}
+
+type SequenceDiscountCondition struct {}
+func NewSequenceDiscountCondition() *SequenceDiscountCondition {
+	return &SequenceDiscountCondition{}
+}
+
+type PeriodDiscountCondition struct {}
+func NewPeriodDiscountCondition() *PeriodDiscountCondition {
+	return &PeriodDiscountCondition{}
+}
--- object/ch2-oop/discount_policy.go
+++ object/ch2-oop/discount_policy.go
@@ -1,3 +1,36 @@
 package main
 
-type DiscountPolicy interface {}
+import "errors"
+
+type DiscountPolicy struct {
+	discountConditions []DiscountCondition
+}
+
+type AmountDiscountPolicy struct {
+	DiscountPolicy
+	amount float32
+}
+
+func NewAmountDiscountPolicy(amount float32, discountConditions []DiscountCondition) *AmountDiscountPolicy {
+	adp := &AmountDiscountPolicy{
+		amount: amount,
+	}
+	adp.discountConditions = discountConditions
+	return adp
+}
+
+type PercentDiscountPolicy struct {
+	DiscountPolicy
+	percent float32
+}
+
+func NewPercentDiscountPolicy(percent float32, discountConditions []DiscountCondition) (*PercentDiscountPolicy, error) {
+	if percent > 1.0 || percent < 0.0 {
+		return nil, errors.New("invalid percent")
+	}
+	pdp := &PercentDiscountPolicy{
+		percent: percent,
+	}
+	pdp.discountConditions = discountConditions
+	return pdp, nil
+}
