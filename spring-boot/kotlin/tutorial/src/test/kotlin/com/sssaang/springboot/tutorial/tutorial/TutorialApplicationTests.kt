8be65addf424d80f37b6829acc2f1519feb52392
Sangmin Kim <sangmin.kim@business-canvas.com>
2021-11-10 23:53:49 +0900

    init

---
+++ spring-boot/kotlin/tutorial/src/test/kotlin/com/sssaang/springboot/tutorial/tutorial/TutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sssaang.springboot.tutorial.tutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
