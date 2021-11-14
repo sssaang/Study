f23b68b675b1c86c1c9ad4e600b98cd21884a2a5
Sangmin Kim <sangmin.kim@business-canvas.com>
2021-11-14 23:03:19 +0900

    feat: add get all banks rest api point

--- spring-boot/kotlin/tutorial/src/main/kotlin/com/sssaang/springboot/tutorial/tutorial/controller/BankController.kt
+++ spring-boot/kotlin/tutorial/src/main/kotlin/com/sssaang/springboot/tutorial/tutorial/controller/BankController.kt
@@ -1,7 +1,15 @@
 package com.sssaang.springboot.tutorial.tutorial.controller
 
+import com.sssaang.springboot.tutorial.tutorial.model.Bank
+import com.sssaang.springboot.tutorial.tutorial.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class BankController {
+@RequestMapping("api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks(): Collection<Bank> = service.getBanks()
 }
